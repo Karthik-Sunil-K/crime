57d7dd2ddcb95bdd71a939ab58c29ccee50cf2f7
webdesign4alll <asbupsilonviroidno1@gmail.com>
2021-05-17 20:21:30 +0530

    added all basic frames level1

---
+++ script.js
@@ -0,0 +1,380 @@
+var dialogue = [
+
+    // dialogue1 by ernst and dialogue 2 by cecil
+    [`SO...WHAT DO YOU THINK?`, `NOTHING SOLID YET, LET’S SCOUT THE AREA FIRST.`],
+    [`ALRIGHT AMIGO, LET'S GO.`, ``],
+    // scene changes to backyard
+    [`WELL...CECIL..`, `YEAH I KNOW WHAT YOU ARE THINKING. THE MURDERER MIGHT
+    HAVE USED THIS PATH.`],
+    [`IT MAKES SENSE ONLY THEN... TAKE THE TWELVE I WILL BE YOUR
+    SIX.`, ``],
+
+    // murder spot
+    [``, `DAMN! WHAT IS THIS ALL ABOUT?`],
+    [`IT SEEMS LIKE BOTH OF THEM ATTACKED EACH OTHER
+    WITH SHARP AND HEAVY WEAPONS.
+    STRANGE ENOUGH…. BOTH THE BODIES
+    HAVE SUFFERED FROM THE SAME IMPACT.`, `SO YOU THINK THEY KILLED EACH OTHER...IF SO, THEN WHO DID
+    CUT-OPEN THE FENCE?`],
+    [`WE MUST FIGURE IT OUT. I’M SURE THAT ONE AMONG THESE WEAPONS IS NOT
+    DIRECTLY INVOLVED IN THIS CRIME, AS IT DOESN’T SEEM TO TRIGGER MY
+    ARTISTIC SENSE.`, `YEAH... I REMEMBER, CRIME IS AN ART.`],
+    [`SI MI AMIGO.YOU GET MY POINT. SO NOW YOU NEED TO FIND OUT THE
+    ODD WEAPON. IT WILL LEAD US TO SOMEONE`, `I’M ON TO IT.`],
+
+    // murder spot--clue must be provided
+
+    // ernst and agent reed speaks out
+    // dialogue 1 ernest dialogue 2 cecil here #8 reed
+    [`YOUR OBSERVATIONS WERE ABSOLUTELY
+    CORRECT.HEY REED, COULD YOU PLEASE BRING
+    THAT WEAPON?`, ``],
+    // #9 its cecil at dialogue 2
+    [`OHH! I'M DAMN SURE THIS AIN'T HUMAN BLOOD.WHO EVER DID THIS IS
+    PRETTY CUNNING.`, `WHAT!!??`],
+    [`YEAH! IT’S GETTING COMPLICATED. SEE THE WAY THEIR SKULLS ARE
+    BROKEN. AND LOOK AT THOSE TWO. THEY COULDN'T HAVE LIFTED THE
+    HEAVY WEAPON IF THEY WANTED TO.`, ``],
+    [`STRIKING EACH OTHER IS OUT OF
+    THE QUESTION.THERE MUST HAVE BEEN A THIRD PERSON, POSSIBLY
+    THE ONE WHO COMMITTED THIS HEINOUS ACT.`, `THAT'S THE CASE , HE WILL NEED AN INSIDER.`],
+
+    // dialogue 1 ernest dialogue 2 reed #12
+    [`A NATIVE?`, `HE'S GOT A POINT. SOMEONE TRIGGERED THE FIRE ALARM AND ALL OF
+    A SUDDEN THOSE PATIENTS WENT CRAZY HIGH!.`],
+    [`HMMH! THEN WHAT ARE WE WAITING FOR, ROUND UP ALL THE STAFF`, ``],
+    // staffroom
+    // dialogue 1 ernest dialogue 2 ruben #14
+    [`RUBEN BROWN..! NICE NAME. SO RB I ONLY WANT TO KNOW A FEW
+    THINGS. WHAT HAPPENED LAST NIGHT?`, `I CAME HERE AT 9 PM FOR MY NIGHT SHIFT . ALTHOUGH I WAS NOT
+    SUPPOSED TO BE HERE YESTERDAY , I WAS ON ADD-ON DUTY...`],
+
+    [`RUBEN... OH CHICO ! CUT IT SHORT.`, `AAH… I WAS HERE RIGHT AT THE MOMENT WHEN THE FIRE ALARM
+    WAS TRIGGERED . I RUSHED TO THE SECOND FLOOR TO SEE THE
+    PATIENTS OUT OF THEIR CELLS . I STILL WONDER HOW THEY DID IT.`],
+
+    // dialogue 1 ernest dialogue2 maddison #16
+    [`MADDISON ASHWELL…HOW ABOUT YOU , WHAT HAPPENED
+    YESTERDAY ?`, `I’M IN-CHARGE OF SECURITY FOOTAGE SURVEILLANCE. WELL I
+    TOO HEARD THAT FREAKING SOUND . I WENT THROUGH ALL THE
+    CAMERAS AND FOUND THAT THE 4TH ONE WAS NOT FUNCTIONING.`],
+    [`WHAT WAS ITS STATUS BEFORE THAT ?`, `PERFECTLY WORKING...`],
+    // dialogue 1 ernst dialogue 2 max #18
+    [`HEY.. WHERE’S YOUR STAFF TAG?`, `I THINK I FORGOT TO TAKE IT . AAAHM... I’M MAX POTTERFIELD.`],
+    [`SO WHAT ABOUT YOU MAX ? WHAT DID YOU SEE?`, `I WAS ON THE THIRD FLOOR IN THE STOREROOM DOING MY JOB .
+    I TRIED TO GET OUT OF THERE WHEN I HEARD THE NOISE BUT I
+    GOT CAUGHT IN PANIC ATTACK .`],
+    [``, `IT TOOK ME A FEW MINUTES TO
+    GET HOLD OF MYSELF ,AFTER WHICH I RUSHED DOWNSTAIRS
+    ONLY TO FIND THOSE BODIES .I SHOULD HAVE BEEN THERE.`],
+
+    [`HEY MAX! RELAX, DON’T STRESS YOURSELF TOO MUCH.`, `IF YOU LET ME, I WANT TO TAKE MY PILLS.`],
+    [`OF COURSE YOU CAN.`, ``],
+
+    // dialogue 1 ernst dialogue 2 david #23
+    [`DAVID BELL... SO, I EXPECT YOU MIGHT HAVE SEEN SOMETHING
+    THE OTHERS DIDN’T . TELL ME!`, `I WAS DOWNSTAIRS NEAR THE SECURITY GUARD’S BUILDING. BUT AS I
+    WAS ON MY WAY , THE AGITATED PATIENTS ATTACKED ME FROM
+    NOWHERE AND BROKE MY ARMS. AWW...THAT HURTS MAN!`],
+    [`LO SIENTO DAVID , MY BAD .`, ``],
+    // #25
+    [`WHAT DO YOU THINK ? WHO IS IT?`, ``],
+    [``,``],
+    // 27 rubbenbrown as suspect
+    // dialogue 1 ernst dialogue 2 cecil
+    [`WHAT! WHY HIM? IT’S PRETTY EVIDENT THAT HE WAS IN THE PATIENT
+    WARD AND THERE ARE NO PSYCHOLOGICAL LOOPHOLES.`, `I THINK IT’S RUBEN BROWN!`],
+    [` HIS EYE
+    MOVEMENTS SUGGEST THAT HE WAS TELLING THE TRUTH. YOU
+    REALLY OUGHT TO IMPROVE IN THIS CECIL.`, ``],
+    [``, ``],
+
+    // 30 maddison as suspect
+    // dialogue 1 ernst dialogue 2 cecil
+
+    [`WHAT! WHY HER ? IT’S PRETTY EVIDENT THAT SHE WAS IN THE
+    SURVEILLANCE ROOM.`, `I THINK IT’S MADDISON ASHWELL!`],
+
+    [`AND THERE ARE NO PSYCHOLOGICAL
+    LOOPHOLES. HER EYE MOVEMENTS SUGGEST THAT SHE WAS
+    TELLING THE TRUTH. YOU REALLY OUGHT TO IMPROVE IN THIS CECIL.`, ``],
+    [``, ``],
+    // 33 max as suspect 
+    [`IT MAKES SENSE.`, `I THINK IT’S MAX!`],
+    [`I DO HAVE A STRONG
+    SUSPICION ON HIM. HE WAS NOT ON THE
+    THIRD, THIS POOR GUY LEFT THE BROKEN
+    PIECE OF HIS ID TAG AT THE SPOT.`, ``],
+    [`I
+    SUPPOSE THINGS DIDN’T GO SMOOTHLY AS
+    PLANNED, DAVID’S PRESENCE MIGHT HAVE
+    INTERRUPTED THEM MOREOVER DAVID’S
+    PULSE RATE WAS NORMAL HE WASN’T
+    LYING.
+    `, ``],
+    // 37 dialogue 1 ernst dialogue 2 cecil
+    [`REED ... TAKE MAX DOWNSTAIRS!`, `SURE. AFTER YOU, SIR.`],
+    [``,``],
+
+    // 38 david as suspect dialogue 1 ernst dialogue 2 cecil
+    [`YOU MADE A TOUGH CALL. IT’S TRUE THAT HE WAS NEAR TO THE
+    SPOT.`, `I THINK IT’S DAVID`],
+    [`BUT THAT DOESN’T PROVE THAT HE’S THE CULPRIT.
+    MULTIPLE REDDISH SCARS ON HIS HANDS ARE MAYBE BECAUSE HE
+    TRIED TO MANAGE THE PATIENTS WHO GOT DOWNSTAIRS . ALSO, I
+    DID CHECK HIS PULSE . IT WAS NORMAL`, `WHY ARE WE MISSING LINK?`],
+    [`I GOT SOMEONE’S BROKEN TAG
+    FROM THE SPOT. YEAH! IT’S MAX!!`, ``],
+    // 42 dialogue 1 ernst dialogue 2 cecil
+    [`REED... TAKE MAX DOWNSTAIRS!`, `SURE. AFTER YOU, SIR.`],
+
+    // #43  42 variable count = 42 dialogue 1 ernst dialogue 2 max
+    [`HEY MAX! YOU NEED TO TELL US EVERYTHING. THE TRUTH THIS TIME.
+    DID YOU KILL THEM ? WHY DID YOU KILL THEM ? WHO HIRED YOU ?
+    WHO’S THAT GHOSTLY GUY ? IS THAT GUY YOUR BOSS? IS THIS PART
+    OF SOMETHING BIG ?`, 'he is silent.......................'],
+
+    [`OH CHICO I HATE SILENCE. I DON’T WANT TO GET ANYTHING
+    WRONG..OR DO YOU WANT ME TO CLEAR EVERYTHING?`, 'really'],
+
+    [`MAX , YOU WERE ON THE SECOND FLOOR WHEN RUBEN TOOK CHARGE.
+    YOU WERE THE ONE WHO LET THE PATIENTS OUT, WEREN’T YOU? YOU
+    ALSO SCANNED THE AREA FOR OTHER STAFFS AND TRIGGERED THE FIRE
+    ALARM WHEN THE COAST WAS CLEAR .`, ``],
+
+    [` THAT GAVE ENOUGH TIME TO
+    BRING VICTIMS TO THE MURDER SPOT , DIDN’T ? JUST LIKE YOUR BOSS
+    PLANNED , AM I RIGHT ?`,``],
+
+    [`SO WHO IS YOUR BOSS , WHY DO IT NOW ? WHY DO IT HERE ? DON’T SAY
+    THAT THEY WERE RANDOMLY FIGHTING EACH OTHER . WHAT ACTUALLY
+    TRIGGERED THE PATIENTS ?`, `HE BUILT THEM… NOW HE’S DEMOLISHING THEM . THE ARMY IS GOING TO
+    DIE`],
+
+    // dialogue 1 ernst dialogue 2 cecil
+    [``,`HE’S DEAD!`]
+
+
+
+];
+var left = document.getElementById('left-dialogue');
+var right = document.getElementById('right-dialogue');
+var sceneOuter = document.getElementById('scene-outer');
+var count = -1;
+
+
+function display() {
+  
+
+    if (count == 26) {
+        sceneOuter.innerHTML = `
+        <div class = 'row mx-auto'>
+        <div class = 'col-12 mx-auto'>
+
+        <h4 class = 'text-center text-capitalize'>who it can be ?</h4>
+        </div>
+        </div>
+
+
+        <div class = 'row mx-auto'>
+        <div class = 'col-12 mx-auto'>
+
+        <span>MAX: <input type="checkbox" name="max" id="max"></span><br>
+        <span>DAVID: <input type="checkbox" name="david" id="david"></span><br>
+        <span>MADDISON: <input type="checkbox" name="maddison" id="maddison"></span><br>
+        <span>RUBBEN: <input type="checkbox" name="rubben" id="rubben" ></span>
+
+
+        </div>
+        </div>`
+
+
+        var maxDialogue = document.getElementById('max');
+        var davidDialogue = document.getElementById('david');
+        var madDialogue = document.getElementById('maddison');
+        var rubDialogue = document.getElementById('rubben');
+        maxDialogue.addEventListener("click", function () {
+
+
+
+            sceneOuter.innerHTML = `
+    
+    <img src="img/asylum.jpg" alt="" class="img-fluid" id="scene">
+    <div class="row mx-auto">
+      <div class="col-12 mx-auto mt-4 pt-2 text-center">
+          <button class='btn' id = 'prev' onclick="bwd()"><i class="fa fa-arrow-left" aria-hidden="true"></i> Prev</button>
+          <button class='btn' onclick="fwd()">Next <i class="fa fa-arrow-right" aria-hidden="true"></i></button>
+    
+    
+      </div>
+    </div>
+    
+    `
+            count = 32;
+
+        })
+
+        davidDialogue.addEventListener("click", function () {
+
+
+
+            sceneOuter.innerHTML = `
+        
+        <img src="img/asylum.jpg" alt="" class="img-fluid" id="scene">
+        <div class="row mx-auto">
+          <div class="col-12 mx-auto mt-4 pt-2 text-center">
+              <button class='btn'id = 'prev' onclick="bwd()"><i class="fa fa-arrow-left" aria-hidden="true"></i> Prev</button>
+              <button class='btn' onclick="fwd()">Next <i class="fa fa-arrow-right" aria-hidden="true"></i></button>
+        
+        
+          </div>
+        </div>
+        
+        `
+            count = 37;
+
+        })
+
+
+        madDialogue.addEventListener("click", function () {
+
+
+
+            sceneOuter.innerHTML = `
+            
+            <img src="img/asylum.jpg" alt="" class="img-fluid" id="scene">
+            <div class="row mx-auto">
+              <div class="col-12 mx-auto mt-4 pt-2 text-center">
+                  <button class='btn'id = 'prev' onclick="bwd()"><i class="fa fa-arrow-left" aria-hidden="true"></i> Prev</button>
+                  <button class='btn' onclick="fwd()">Next <i class="fa fa-arrow-right" aria-hidden="true"></i></button>
+            
+            
+              </div>
+            </div>
+            
+            `
+            count = 29;
+
+        })
+
+
+        rubDialogue.addEventListener("click", function () {
+
+
+
+            sceneOuter.innerHTML = `
+                
+                <img src="img/asylum.jpg" alt="" class="img-fluid" id="scene">
+                <div class="row mx-auto">
+                  <div class="col-12 mx-auto mt-4 pt-2 text-center">
+                      <button class='btn'id = 'prev' onclick="bwd()"><i class="fa fa-arrow-left" aria-hidden="true"></i> Prev</button>
+                      <button class='btn' onclick="fwd()">Next <i class="fa fa-arrow-right" aria-hidden="true"></i></button>
+                
+                
+                  </div>
+                </div>
+                
+                `
+            count = 26;
+
+        })
+
+    }
+
+        left.innerHTML = dialogue[count][0].toLowerCase();
+        right.innerHTML = dialogue[count][1].toLowerCase();
+        scene = document.getElementById('scene').src;
+    
+  
+
+    if(count == 27 || count == 30 || count == 33 || count == 38 || count == 40 ){
+      
+        document.getElementById('prev').style.display = 'none';
+    }else if(count == 29 || count == 32 || count == 37){
+        alert(`${count}`)
+        count = 39;
+        document.getElementById('prev').style.display = 'inline-block';
+
+    }
+    else{
+        document.getElementById('prev').style.display = 'inline-block';
+    }
+
+
+    if(count == 25){
+        alert(`count is ${count}`)
+    }
+     if (count >= 23) {
+    // alert(`count ${count}`)
+
+        document.getElementById('right-char').src = 'img/david.jpg';
+        document.getElementById('scene').src = 'img/david-staff.jpg';
+    }
+    else if (count >= 18) {
+        document.getElementById('right-char').src = 'img/max.jpg';
+        document.getElementById('scene').src = 'img/max-staff.jpg';
+    }
+    else if (count >= 16) {
+        document.getElementById('right-char').src = 'img/maddison.jpg';
+        document.getElementById('scene').src = 'img/maddison-staff.jpg';
+    }
+    else if (count >= 14) {
+        document.getElementById('right-char').src = 'img/rube.jpg';
+        document.getElementById('scene').src = 'img/rube-staff.jpg';
+    }
+    else if (count >= 12) {
+        document.getElementById('right-char').src = 'img/reed.jpg';
+    }
+    else if (count >= 9) {
+        document.getElementById('right-char').src = 'img/char1.jpg';
+    }
+    else if (count >= 8) {
+        document.getElementById('right-char').src = 'img/reed.jpg'
+    }
+    else if (count >= 4) {
+        document.getElementById('scene').src = 'img/hover1.png';
+        document.getElementById('right-char').src = 'img/char1.jpg';
+
+    } else if (count >= 2) {
+        document.getElementById('scene').src = 'img/backyard.jpg';
+    }
+    else {
+        document.getElementById('scene').src = 'img/asylum.jpg';
+        document.getElementById('right-char').src = 'img/char1.jpg';
+
+    }
+}
+function fwd() {
+
+    count = count + 1;
+    
+    display();
+
+}
+
+function bwd() {
+
+    if (count < -1) {
+
+        count = 0;
+
+    }
+
+    count = count - 1;
+    display();
+
+}
+
+
+
+
+
+var nav = document.getElementById('outer-nav');
+nav.addEventListener("mouseenter", function () {
+    var expand = document.getElementById('nav')
+    expand.style.animation = 'navfull 2s forwards';
+})
+nav.addEventListener("mouseleave", function () {
+    var expand = document.getElementById('nav')
+    expand.style.animation = 'navsmall 2s forwards';
+})
