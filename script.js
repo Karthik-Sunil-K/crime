486ab94659b01e900b6470c58292483eb3423645
webdesign4alll <asbupsilonviroidno1@gmail.com>
2021-05-22 14:12:00 +0530

    added end of level1

--- script.js
+++ script.js
@@ -154,7 +154,8 @@ var dialogue = [
     DIE`],
 
     // dialogue 1 Ernest dialogue 2 cecil
-    [``, `HE’S DEAD!`]
+    [``, `HE’S DEAD!`],
+    [``, ``]
 
    
 
@@ -183,8 +184,13 @@ function display() {
 if(count >= 42){
     document.getElementById('right-char').src = `img/max.png`;
     char2.innerHTML = 'MAX';
+if(count == 48){
+    document.getElementById('scene-script').style.display = 'block';
+    char2.innerHTML = 'CECIL';
 
-    if(count == 46){
+    document.getElementById('scene-script').innerHTML = `<h3 class = "text-center">END OF LEVEL ONE</h3>`;
+}
+    else if(count == 46){
         document.getElementById('scene-script').style.display = 'block';
         document.getElementById('scene-script').innerHTML = `
         Without warning blood surged out of his nose, mouth and ears.
